612aff8ab4a65fe44ab4fe3ce10802fb52b95d23
GoDotDotDot <chukui0202@gmail.com>
2024-07-31 22:43:57 +0800

    feat: add get reddit community rss feed

---
+++ lib/routes/reddit/community.ts
@@ -0,0 +1,48 @@
+import { Route } from '@/types';
+import extractFromHtml from '@/utils/html-to-md';
+import parser from '@/utils/rss-parser';
+
+export const route: Route = {
+    path: '/r/:community?',
+    categories: ['social-media'],
+    example: '/reddit/r/ArtificialInteligence',
+    parameters: {
+        community: 'reddit community name',
+    },
+    features: {
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: 'Reddit Community',
+    maintainers: ['Questflow'],
+    handler,
+    radar: [
+        {
+            source: ['reddit.com/r/:community'],
+            target: '/r/:community',
+        },
+    ],
+};
+
+async function handler(ctx) {
+    const community = ctx.req.param('community');
+    const rssUrl = `https://www.reddit.com/r/${community}/.rss`;
+    const feed = await parser.parseURL(rssUrl);
+
+    return {
+        title: feed.title,
+        link: feed.link,
+        item: feed.items.map((item) => ({
+                title: item.title,
+                pubDate: item.pubDate,
+                link: item.link,
+                category: item.categories,
+                description: extractFromHtml(item.content),
+                // @ts-ignore
+                author: item.author as string,
+            })),
+    };
+}
